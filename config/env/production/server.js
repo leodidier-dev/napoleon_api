8e64a23bfbd0e767f4c69944b30f6cd2c64ff842
leo <leo@60fps.fr>
2022-12-27 12:43:20 +0100

    fx

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,5 @@
 module.exports = ({ env }) => ({
   proxy: true,
-  host: env("APP_HOST", "0.0.0.0"),
-  port: env.int("NODE_PORT", 1337),
   url: env("APP_URL"), // replaces `host` and `port` properties in the development environment
   app: {
     keys: env.array("APP_KEYS"),
