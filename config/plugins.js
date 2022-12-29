9c48930342a737906a0fc501ffc8690d96bdeebe
leo <leo@60fps.fr>
2022-12-29 18:12:39 +0100

    fx

--- config/plugins.js
+++ config/plugins.js
@@ -1,71 +1,67 @@
-module.exports = ({ env }) =>  ({
-    'navigation': { enabled: false },
-    'graphql': { enabled: true },
-    'ckeditor': {
-        enabled: true,
-        config:{
-           editor:{
-            toolbar: {
-               items: [
-                 'paragraph',
-                 'heading1',
-                 'heading2',
-                 'heading3',
-                 '|',
-                 'bold',
-                 'italic',
-                 'underline',
-                 'strikethrough',
-                 '|',
-                 'bulletedList',
-                 'numberedList',
-                 '|',
-                 'StrapiMediaLib',
-                 'insertTable',
-                 'blockQuote',
-                 'link',
-                 '|',
-                 "fullScreen",
-                 'undo',
-                 'redo'
-               ]
-             },
-             image: {
-               toolbar: [
-                 'toggleImageCaption',
-                 'imageTextAlternative'
-               ]
-             },
-             // https://ckeditor.com/docs/ckeditor5/latest/features/table.html
-             table: {
-               contentToolbar: [
-                 'tableColumn',
-                 'tableRow',
-                 'mergeTableCells',
-                 'tableCellProperties',
-                 'tableProperties',
-                 'toggleTableCaption'
-               ]
-             },
-           }
-         }
-       },
-
-       upload: {
-        config: {
-          provider: 'cloudinary',
-          providerOptions: {
-            cloud_name: env('CLOUDINARY_NAME'),
-            api_key: env('CLOUDINARY_KEY'),
-            api_secret: env('CLOUDINARY_SECRET'),
-          },
-          actionOptions: {
-            upload: {},
-            uploadStream: {},
-            delete: {},
-          },
+module.exports = ({ env }) => ({
+  navigation: { enabled: false },
+  graphql: { enabled: true },
+  ckeditor: {
+    enabled: true,
+    config: {
+      editor: {
+        removePlugins: ["FontColor"],
+        toolbar: {
+          items: [
+            "paragraph",
+            "heading1",
+            "heading2",
+            "heading3",
+            "|",
+            "bold",
+            "italic",
+            "underline",
+            "strikethrough",
+            "|",
+            "bulletedList",
+            "numberedList",
+            "|",
+            "StrapiMediaLib",
+            "insertTable",
+            "blockQuote",
+            "link",
+            "|",
+            "fullScreen",
+            "undo",
+            "redo",
+          ],
+        },
+        image: {
+          toolbar: ["toggleImageCaption", "imageTextAlternative"],
+        },
+        // https://ckeditor.com/docs/ckeditor5/latest/features/table.html
+        table: {
+          contentToolbar: [
+            "tableColumn",
+            "tableRow",
+            "mergeTableCells",
+            "tableCellProperties",
+            "tableProperties",
+            "toggleTableCaption",
+          ],
         },
       },
+    },
+  },
 
-
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
 });
