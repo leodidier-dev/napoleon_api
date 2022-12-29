641ef8bdf47843ee4504994b7072f8d93a8ee2e7
leo <leo@60fps.fr>
2022-12-29 18:35:41 +0100

    fx

--- config/plugins.js
+++ config/plugins.js
@@ -5,7 +5,6 @@ module.exports = ({ env }) =>  ({
         enabled: true,
         config:{
            editor:{
-            removePlugins: ['FontColor'],
             toolbar: {
                items: [
                  'paragraph',
